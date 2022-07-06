3ccf8fae5b029ad4dad1683d7cec3a8fa45423e8
garretts8 <gar21085@byui.edu>
2022-07-05 21:49:37 -0600

    Adding initial files

---
+++ Pong/game/scripting/load_assets_action.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from game.scripting.action import Action
+
+
+class LoadAssetsAction(Action):
+
+    def __init__(self, audio_service, video_service):
+        self._audio_service = audio_service
+        self._video_service = video_service
+
+    def execute(self, cast, script, callback):
+        self._audio_service.load_sounds("pong/assets/sounds")
+        self._video_service.load_fonts("pong/assets/fonts")
+        self._video_service.load_images("pong/assets/images")
+        
