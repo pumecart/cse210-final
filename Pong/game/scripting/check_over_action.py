7d1ffba7aee2c661a4454a47bd2477124c06a80e
Eldon Martinez <mar18118@byui.edu>
2022-07-06 23:42:36 -0600

    bricks error fix

--- Pong/game/scripting/check_over_action.py
+++ Pong/game/scripting/check_over_action.py
@@ -1,15 +1,15 @@
-from constants import *
-from game.scripting.action import Action
+# from constants import *
+# from game.scripting.action import Action
 
 
-class CheckOverAction(Action):
+# class CheckOverAction(Action):
 
-    def __init__(self):
-        pass
+#     def __init__(self):
+#         pass
         
-    def execute(self, cast, script, callback):
-        bricks = cast.get_actors(BRICK_GROUP)
-        if len(bricks) == 0:
-            stats = cast.get_first_actor(STATS_GROUP)
-            stats.next_level()
-            callback.on_next(NEXT_LEVEL)
+#     def execute(self, cast, script, callback):
+#         bricks = cast.get_actors(BRICK_GROUP)
+#         if len(bricks) == 0:
+#             stats = cast.get_first_actor(STATS_GROUP)
+#             stats.next_level()
+#             callback.on_next(NEXT_LEVEL)
