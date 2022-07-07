72f14bd05f1704761ff0ca961983294ed30889c9
Eldon Martinez <mar18118@byui.edu>
2022-07-07 00:27:38 -0600

    fixed racket up/down movement

--- Pong/game/scripting/move_racket_action.py
+++ Pong/game/scripting/move_racket_action.py
@@ -13,14 +13,19 @@ class MoveRacketAction(Action):
         body = racket1.get_body()
         velocity = body.get_velocity()
         position = body.get_position()
-        x = position.get_x()
+        # x = position.get_x()
+        y = position.get_y()
         
         position = position.add(velocity)
 
-        if x < 0:
-            position = Point(0, position.get_y())
-        elif x > (SCREEN_WIDTH - RACKET_WIDTH):
-            position = Point(SCREEN_WIDTH - RACKET_WIDTH, position.get_y())
+        # if x < 0:
+        #     position = Point(0, position.get_y())
+        # elif x > (SCREEN_WIDTH - RACKET_WIDTH):
+        #     position = Point(SCREEN_WIDTH - RACKET_WIDTH, position.get_y())
+        if y < 0:
+            position = Point(position.get_x(), 0)
+        elif y > (SCREEN_HEIGHT - RACKET_HEIGHT):
+             position = Point(position.get_x(), SCREEN_HEIGHT - RACKET_HEIGHT)
             
         body.set_position(position)
         
