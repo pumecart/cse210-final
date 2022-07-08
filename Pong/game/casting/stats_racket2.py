1a270f8b2faa987c51f2c48f178c373f5977a9fa
pumecart <car21132@byui.edu>
2022-07-07 20:06:13 -0400

    scores- 90% there

--- Pong/game/casting/stats_racket2.py
+++ Pong/game/casting/stats_racket2.py
@@ -1,8 +1,9 @@
 from constants import *
 from game.casting.actor import Actor
+from game.casting.stats_racket1 import Stats_Racket1
 
 
-class Stats(Actor):
+class Stats_Racket2(Stats_Racket1):
     """The game stats."""
 
     def __init__(self, debug = False):
